85271cf14d6a8543e23384ff9e07cf6018d347b7
ILPATHERA <sommesi1@students.zhaw.ch>
2023-09-15 19:07:34 +0200

    last changes 140923

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import smtplib
 
 app = Flask(__name__)
 
@@ -11,13 +12,40 @@ def index():
 
 @app.route('/submit_form', methods=['POST'])
 def contact():
-    # Hier würde Ihre E-Mail-Versand-Logik sein
-    # ...
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
 
-    # Wenn die Nachricht erfolgreich gesendet wurde
-    global confirmation_message
-    confirmation_message = 'Deine Nachricht wurde erfolgreich gesendet!'
-    return redirect(url_for('confirmation'))
+    # E-Mail-Versand
+    smtp_server = "smtp.office365.com"  # Beispiel: smtp.gmail.com für Gmail
+    smtp_port = 587  # Beispielport für TLS
+    smtp_user = "patheracoaching@outlook.com"
+    smtp_password = "Coaching2023!"
+
+    subject = f"Neue Nachricht von {name}"
+    body = f"Name: {name}\nE-Mail: {email}\n\nNachricht:\n{message}"
+
+    try:
+        # Verbindung zum SMTP-Server herstellen
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        
+        # Anmeldung am SMTP-Server
+        server.login(smtp_user, smtp_password)
+        
+        # E-Mail senden
+        server.sendmail(smtp_user, ['patheracoaching@outlook.com'], body)
+        
+        # Verbindung schließen
+        server.quit()
+        
+        # Wenn die Nachricht erfolgreich gesendet wurde
+        global confirmation_message
+        confirmation_message = 'Deine Nachricht wurde erfolgreich gesendet!'
+        return redirect(url_for('confirmation'))
+    except Exception as e:
+        print(f"Fehler beim Senden der E-Mail: {str(e)}")
+        return redirect(url_for('error'))
 
 @app.route('/confirmation')
 def confirmation():
